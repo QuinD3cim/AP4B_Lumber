9fba0c99f48b9a76a9628b72ef1442180d38acea
lsacienne <alexandre.viala@outlook.com>
2022-05-18 17:43:20 +0200

    Add of main window

--- src/lumber_jack/Main.java
+++ src/lumber_jack/Main.java
@@ -1,9 +1,21 @@
 package lumber_jack;
 
-public class Main {
-    public static int main(String[] args) {
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
 
-        return 0;
+import lumber_jack.controller.MenuController;
+import lumber_jack.model.IndustryModel;
+
+public class Main extends JFrame {
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main() {
+        new IndustryModel();
+        new MenuController();
+        setMinimumSize(new Dimension(800, 800));
+        setVisible(true);
     }
 }
