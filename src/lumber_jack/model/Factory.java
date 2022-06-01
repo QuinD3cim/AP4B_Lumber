132d3b11d4feeebee2c54a4de53c99e2bc72a868
lsacienne <alexandre.viala@outlook.com>
2022-06-01 21:46:05 +0200

    :sparkles: Add of Factory class

---
+++ src/lumber_jack/model/Factory.java
@@ -0,0 +1,12 @@
+package lumber_jack.model;
+
+import java.util.ArrayList;
+
+public class Factory {
+    protected float size;
+    protected ArrayList<FactoryPlot> plots;
+
+    public void buyPlot() {
+
+    }
+}
